d38aeebd3325fa23528d317aa035b2c0ad9ebb3c
Mico <m.enriquez4060@hotmail.com>
2022-12-12 13:40:53 -0800

    finals

--- js/main.js
+++ js/main.js
@@ -1,18 +1,60 @@
-//https://tinyurl.com/dynamic-html-checker
-document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
-    
-document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
-    
-    //set current year in span with id of this-year
-    let d = new Date(); let thisYear = d.getFullYear();
-    document.getElementById("this-year").innerHTML = thisYear;
 
-//manages mobile nav 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+function highlight (el){
+    //el.style.backgroundColor='blue';
+    
+    if(el.style.backgroundColor == 'white'){//white turn to yellow
+    el.style.backgroundColor='yellow';
+    }else{//turn to white
+        el.style.backgroundColor='white';
     }
-}   
+}
+function myAlert(){
+    alert("I'm clicked");
+}
+function highlightActor(actor){
+    let spans = document.querySelectorAll("#play span");
+    for(const mySpan of spans){
+
+        if(actor ==mySpan.dataset.actor){//white turn to yellow
+            mySpan.style.backgroundColor='yellow';
+            }else{//turn to white
+                mySpan.style.backgroundColor='white';
+            }
+
+
+
+
+    }
+    //alert(actor);
+}
+let spans = document.querySelectorAll("#play span");
+ 
+console.log(spans);
+
+for(const mySpan of spans){
+    //mySpan.addEventListener("click",myAlert);
+    //alert(mySpan.dataset.actor);
+
+    mySpan.addEventListener("click",function(ev){
+        highlightActor(mySpan.dataset.actor);
+
+
+    });
+
+
+}
+    
+
+
+
+
+
+
+
+let actors = document.querySelectorAll(".hamlet");
+ 
+console.log(actors);
+ 
+for (let i = 0; i < actors.length; i++) {
+ actors[i].style.backgroundColor = "orange";
+}
